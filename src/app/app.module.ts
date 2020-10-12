c9fd64e7cd300767ad8f934d50918e09e1ded902
mkmdev <mkmdev18@gmail.com>
2020-10-12 12:27:26 +0200

    sketch up skeleton

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { HomeComponent } from './home/home.component';
+import { HomeComponent } from './components/home/home.component';
+import { InputFormComponent } from './components/input-form/input-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    InputFormComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/models/customer.ts
@@ -0,0 +1,7 @@
+export class Customer {
+  name: string;
+  age: number;
+  sex: boolean;
+  height: number;
+  weight: number;
+}
