b8f78f8250571e064ad1dc80727e0c81eb9f21bf
mkmdev <mkmdev18@gmail.com>
2020-10-13 17:11:28 +0200

    optimized imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,12 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { HomeComponent } from './components/home/home.component';
-import { InputFormComponent } from './components/input-form/input-form.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
+import {HomeComponent} from './components/home/home.component';
+import {InputFormComponent} from './components/input-form/input-form.component';
 import {FormsModule} from '@angular/forms';
 
 @NgModule({
