4c69f37cc2ad7a6659d60f8ac3a997306e591dd2
mkmdev <mkmdev18@gmail.com>
2020-10-19 07:30:14 +0200

    +tippek, hogyan lehetne kijavítani

--- src/app/components/result/result.component.ts
+++ src/app/components/result/result.component.ts
@@ -13,6 +13,7 @@ export class ResultComponent implements OnInit {
   height: number;
   weight: number;
   bmiNumber: number;
+  bmiGroup: string;
   bmiGroups = [
     'súlyos soványság',
     'mérsékelt soványság',
@@ -23,7 +24,6 @@ export class ResultComponent implements OnInit {
     'II. fokú elhízás',
     'III. fokú elhízás',
   ];
-  bmiGroup: string;
 
   constructor(
     private location: Location,
@@ -42,12 +42,12 @@ export class ResultComponent implements OnInit {
   }
 
   goBack(): void {
-    // fixme
+    // fixme tipp: session storage-ba menteni az inputokat és onnan feltölteni?
     this.router.navigateByUrl('');
   }
 
   goHome(): void {
-    // fixme
+    // fixme tipp: activated route paramba reseted=true-val, és az ngOninitben resetelje a formot mindegyik aloldalon
     this.router.navigateByUrl('');
   }
 
